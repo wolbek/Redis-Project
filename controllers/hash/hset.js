b64a32ad42e6e6c29d6088c6b8e4ff199a1d8b0d
crosswalkz <akshaysrikanth451@gmail.com>
2024-01-17 14:31:34 +0530

    first commit

---
+++ controllers/hash/hset.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function hset() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default hset
---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+
+const app = express()
+
+app.listen(8081 , ()=>{
+    console.log("Working fine!!")
+})
