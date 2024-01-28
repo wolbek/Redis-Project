28ac2e8ca124630b91d166105106730c56832548
Aakash Vishwakarma <aakashvish22@gmail.com>
2024-01-28 11:44:38 +0530

    Removed models. Logic in controllers.

--- controllers/hash.js
+++ controllers/hash.js
@@ -1,12 +1,10 @@
-import RedisHash from "../models/hash.js";
-
-const hash = new RedisHash();
+const hash = {}
 
 export const setHashField = async (req, res, next) => {
     try{
         const key = Object.keys(req.body)[0];
         const fields = req.body[key];
-        const response = await hash.hset(key, fields);
+        const response = await hset(key, fields);
 
         return res.status(200).json({response:response});
     }
@@ -14,11 +12,22 @@ export const setHashField = async (req, res, next) => {
         next(err);
     }
 };
+
+const hset = (key, fields) => {
+    return new Promise((resolve, reject) => {
+        try {
+            hash[key] = {...hash[key], ...fields};
+            resolve('Successfully saved.');
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
 
 export const getHashField = async (req, res, next) => {
     try{
         const {key, field} = req.params;
-        const response = await hash.hget(key, field);
+        const response = await hget(key, field);
 
         return res.status(200).json({response:response});
     }
@@ -26,11 +35,25 @@ export const getHashField = async (req, res, next) => {
         next(err);
     }
 };
+
+const hget = (key, field) => {
+    return new Promise((resolve, reject) => {
+        try {
+            if (hash[key] && hash[key][field] !== undefined) {
+                resolve(hash[key][field]);
+            } else {
+                reject(new Error('Not Found'));
+            }
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
 
 export const getHashAllFields = async (req, res, next) => {
     try{
         const key = req.params.key;
-        const response = await hash.hgetall(key);
+        const response = await hgetall(key);
 
         return res.status(200).json({response:response});
     }
@@ -38,12 +61,26 @@ export const getHashAllFields = async (req, res, next) => {
         next(err);
     }
 };
+
+const hgetall = (key) => {
+    return new Promise((resolve, reject) => {
+        try{
+            if (hash[key] !== undefined){
+                resolve(hash[key]);
+            }else{
+                reject(new Error('Not Found'));
+            }
+        } catch(err){
+            reject(err);
+        }
+    });
+}
 
 export const deleteHashField = async (req, res, next) => {
     try{
         const key = req.body.key;
         const fields = req.body.fields;
-        const response = await hash.hdel(key, fields);
+        const response = await hdel(key, fields);
 
         return res.status(200).json({response:response});
     }
@@ -52,10 +89,35 @@ export const deleteHashField = async (req, res, next) => {
     }
 };
 
+const hdel = (key, fields) => {
+    return new Promise((resolve, reject) => {
+        try{
+            if(hash[key]!== undefined){
+
+                for(let field of fields){
+                    if(hash[key][field]=== undefined){
+                        reject(new Error('Some fields does not exist'));
+                    }
+                }
+                for(let field of fields){
+                    delete hash[key][field];
+                }
+                
+                resolve(`Successfully deleted given fields.`);
+
+            }else{
+                reject(new Error('Not Found'));
+            }
+        } catch(err){
+            reject(err);
+        }
+    });
+}
+
 export const findHashLength = async (req, res, next) => {
     try{
         const key = req.params.key;
-        const response = await hash.hlen(key);
+        const response = await hlen(key);
         
         return res.status(200).json({response:response});
     }
@@ -63,3 +125,17 @@ export const findHashLength = async (req, res, next) => {
         next(err);
     }
 };
+
+const hlen = (key) => {
+    return new Promise((resolve, reject) => {
+        try{
+            if (hash[key] !== undefined){
+                resolve(Object.keys(hash[key]).length);
+            }else{
+                reject(new Error('Not Found'));
+            }
+        } catch(err){
+            reject(err);
+        }
+    });
+}
--- controllers/string.js
+++ controllers/string.js
@@ -1,36 +1,59 @@
-import RedisString from "../models/string.js";
-
-const string = new RedisString();
+const string = {};
 
 export const setString = async (req, res, next) => {
     try{
         const key = Object.keys(req.body)[0];
         const value = req.body[key];
-        const response = await string.set(key, value); 
+        const response = await set(key, value); 
 
         return res.status(200).json({response:response});
     }
     catch(err){
         next(err);
     }
+}
+
+const set = (key, value) => {
+    return new Promise((resolve, reject) => {
+        try {
+            string[key] = value;
+            resolve('Successfully saved.');
+        } catch (err) {
+            reject(err);
+        }
+    });
 }
 
 export const getString = async (req, res, next) => {
     try{
         const key = req.params.key;
-        const response = await string.get(key); 
+        const response = await get(key); 
 
         return res.status(200).json({response:response});
     }
     catch(err){
         next(err);
     }
+}
+
+const get = (key) => {
+    return new Promise((resolve, reject) => {
+        try {
+            if (string[key] !== undefined) {
+                resolve(string[key]);
+            } else {
+                reject(new Error('Not Found'));
+            }
+        } catch (err) {
+            reject(err);
+        }
+    });
 }
 
 export const deleteString = async (req, res, next) => {
     try{
         const key = req.body.key;
-        const response = await string.getdel(key); 
+        const response = await getdel(key); 
 
         return res.status(200).json({response:response});
     }
@@ -39,14 +62,46 @@ export const deleteString = async (req, res, next) => {
     }
 }
 
+const getdel = (key) => {
+    return new Promise((resolve, reject) => {
+        try{
+            if(string[key]!== undefined){
+                const deletedValue = string[key];
+                delete string[key];
+                
+                resolve(deletedValue);
+
+            }else{
+                reject(new Error('Not Found'));
+            }
+        } catch(err){
+            reject(err);
+        }
+    });
+}
+
 export const getStringLength = async (req, res, next) => {
     try{
         const key = req.params.key;
-        const response = await string.strlen(key);
+        const response = await strlen(key);
 
         return res.status(200).json({response:response});
     }
     catch(err){
         next(err);
     }
+}
+
+const strlen = (key) => {
+    return new Promise((resolve, reject) => {
+        try{
+            if (string[key] !== undefined){
+                resolve(Object.keys(string[key]).length);
+            }else{
+                reject(new Error('Not Found'));
+            }
+        } catch(err){
+            reject(err);
+        }
+    });
 }
